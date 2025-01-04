476976f96373e084f2ca214e6b14196543c7698b
ArthurSampaio13 <arthur.sampaio@upe.br>
2025-01-04 20:25:21 -0300

    feat: botao de logout

--- livraria/urls.py
+++ livraria/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from django.http import HttpResponse
-from livraria.views import home
+from livraria.views import home, logout_user
 
 urlpatterns = [
     path('', home, name='home'),
+    path('logout/', logout_user),
 ]
