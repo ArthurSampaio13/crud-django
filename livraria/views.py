950ed7e91977977daafa600ef9901e52b7582123
ArthurSampaio13 <arthur.sampaio@upe.br>
2025-01-04 20:25:32 -0300

    feat: logica de login

--- livraria/views.py
+++ livraria/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 def home(request):
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        username = request.POST['usuario']
+        senha = request.POST['senha']
+        
+        user = authenticate(
+            request, 
+            username=username, 
+            password=senha
+        )
+        
+        if user is not None:
+            login(request, user)
+            messages.success(
+                request,
+                "Login realizado com sucesso"
+            )
+            return redirect('home')
+        else:
+            messages.error(
+                request,
+                "Erro na autenticação. Tente novamente!"
+            )
+            return redirect('home')
+    else:
+        return render(request, 'home.html')
+
+def logout_user(request):
+    pass
