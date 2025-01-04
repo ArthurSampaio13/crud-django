35d55342d73b5f2da16e27694f7227a23beff5f1
ArthurSampaio13 <arthur.sampaio@upe.br>
2025-01-04 20:24:35 -0300

    feat: configurando mensagem de error

--- crudDjango/settings.py
+++ crudDjango/settings.py
@@ -1,4 +1,9 @@
 from pathlib import Path
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
