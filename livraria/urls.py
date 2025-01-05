38d7a85326982e7b2028fff25dbc7cebef7650ae
ArthurSampaio13 <arthur.sampaio@upe.br>
2025-01-04 22:18:48 -0300

    feat: rota de register

--- livraria/urls.py
+++ livraria/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from livraria.views import home, logout_user
+from livraria.views import home, logout_user, register
 
 urlpatterns = [
     path('', home, name='home'),
-    path('logout/', logout_user),
+    path('logout/', logout_user, name = "logout"),
+    path('register/', register, name = "register")
 ]
